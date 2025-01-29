3c49d64c2e662c5b238dee1ad80f8c9c5b68e4f7
ibrahim-ibru <mohammedibrahim2k02@gmail.com>
2025-01-29 10:36:01 +0530

    updated

--- serverside/server.js
+++ serverside/server.js
@@ -4,13 +4,9 @@ import connection from "./conection.js"
 import router from "./router.js"
 import cors from "cors"
 
-
-
-
 env.config()
 const app=express()
 app.use(cors());
-// app.use(express.static("../clientside"))
 app.use(express.json())
 app.use("/api",router)
 
